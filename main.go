b08309c39c438ee4e9878c6ac2fa4812b8127099
mpetavy <mpetavy>
2023-10-21 09:22:03 +0200

    initial commit

---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mpetavy/common"
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/mqtt"
+	"log"
+	"time"
+)
+
+func init() {
+	common.Init("gobot", "", "", "", "2023", "gobot", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
+}
+
+func run() error {
+	mqttAdaptor := mqtt.NewAdaptor("tcp://broker.hivemq.com:1883", "pinger1")
+
+	work := func() {
+		mqttAdaptor.On("hello", func(msg mqtt.Message) {
+			fmt.Printf("%+v\n", msg.Payload())
+		})
+
+		data := []byte("Marcel")
+		gobot.Every(1*time.Second, func() {
+			mqttAdaptor.Publish("hello", data)
+		})
+	}
+
+	robot := gobot.NewRobot("mqttBot",
+		[]gobot.Connection{mqttAdaptor},
+		work,
+	)
+
+	robot.AutoRun = false
+
+	var err error
+
+	go func() {
+		err = robot.Start()
+	}()
+
+	time.Sleep(time.Second)
+
+	if common.Error(err) {
+		return err
+	}
+
+	time.Sleep(time.Second * 5)
+
+	log.Printf("stopping server ...\n")
+	err = robot.Stop()
+	if common.Error(err) {
+		return err
+	}
+	log.Printf("server stopped!\n")
+
+	return nil
+}
+
+func main() {
+	common.Run(nil)
+}
